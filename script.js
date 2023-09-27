e8f8da266fdda04f20ad4ca30c24fc6578b68d8d
Mr. Ryt <flaviosobbin04@gmail.com>
2023-09-27 18:38:26 +0000

    br

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const formName = document.getElementById("form_name");
+const form = document.getElementsByTagName("form")[0];
+const complete = document.getElementsByClassName("gratitude")[0];
+
 const formNumber = document.getElementById("form_num");
 const formMonth = document.getElementById("form_month");
 const formYear = document.getElementById("form_year");
 const formCVC = document.getElementById("form_cvc");
+const formBtn = document.getElementById("form_btn");
+
 var cardName = document.getElementById("name");
 const cardNumber = document.querySelectorAll(".num span");
 const cardMonth = document.querySelectorAll(".month span");
@@ -13,7 +18,6 @@ const logName = document.getElementById("log_name");
 const logNumber = document.getElementById("log_num");
 const logDate = document.getElementById("log_date");
 const logCVC = document.getElementById("log_cvc");
-var cvc = document.getElementById("cvc");
 
 formName.addEventListener("input", () => {
   if (formName.value.length <= 22) {
@@ -73,9 +77,9 @@ formYear.addEventListener("keypress", updateYear);
 formYear.addEventListener("keydown", updateYear);
 
 const updateCVC = (event) => {
+  let i = formCVC.value.length;
   let f = formCVC.value;
   for (const e of f) {
-    logName.innerText = e;
     cardCVC[i - 1].innerText = e;
   }
   if (event.key === "Backspace") {
@@ -87,3 +91,80 @@ formCVC.addEventListener("input", updateCVC);
 formCVC.addEventListener("keyup", updateCVC);
 formCVC.addEventListener("keypress", updateCVC);
 formCVC.addEventListener("keydown", updateCVC);
+
+const blankMessage = (form, log) => {
+  form.style.borderColor = "red";
+  log.innerText = "Can't be blank";
+};
+
+const restore = (form, log) => {
+  form.style.borderColor = "var(--lightGrayishViolet)";
+  log.innerText = "";
+};
+
+let a,
+  b,
+  c,
+  d,
+  e = 0;
+
+function authenticate() {
+  if (formName.value === "") {
+    blankMessage(formName, logName);
+    a = 0;
+  } else if (!isNaN(formName.value)) {
+    logName.innerText = "Wrong format, letters only";
+    formName.style.borderColor = "red";
+  } else {
+    restore(formName, logName);
+    a = 1;
+  }
+  if (formNumber.value === "") {
+    blankMessage(formNumber, logNumber);
+    b = 0;
+  } else if (isNaN(formNumber.value)) {
+    logNumber.innerText = "Wrong format, numbers only";
+    formNumber.style.borderColor = "red";
+  } else {
+    restore(formNumber, logNumber);
+    b = 1;
+  }
+  if (formMonth.value === "") {
+    blankMessage(formMonth, logDate);
+    c = 0;
+  } else if (formMonth.value < 1 || formMonth.value > 12) {
+    logDate.innerText = "Must be a valid date";
+    formMonth.style.borderColor = "red";
+  } else {
+    restore(formMonth, logDate);
+    c = 1;
+  }
+  if (formYear.value === "") {
+    blankMessage(formYear, logDate);
+    d = 0;
+  } else if (formYear.value < 0 || formYear.value > 99) {
+    logDate.innerText = "Must be a valid date";
+    formYear.style.borderColor = "red";
+  } else {
+    restore(formYear, logDate);
+    d = 1;
+  }
+  if (formCVC.value === "") {
+    blankMessage(formCVC, logCVC);
+    e = 0;
+  } else if (formCVC.value.length !== 3) {
+    logCVC.innerText = "Invalid";
+    formCVC.style.borderColor = "red";
+  } else {
+    restore(formCVC, logCVC);
+    e = 1;
+  }
+  if (a && b && c && d && e) {
+    form.style.display = "none";
+    complete.style.display = "flex";
+  }
+}
+
+const reset = () => {
+  window.location = window.location.href;
+};
